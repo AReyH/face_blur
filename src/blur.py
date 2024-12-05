0897ba2a8913633b6ec179a185121c5749064e3c
Arturo Rey Haggar <arturo.rey.h@outlook.com>
2024-12-04 20:35:35 -0800

    Added some context on the functions

--- src/blur.py
+++ src/blur.py
@@ -1,2 +1,7 @@
-def blur(sub_matrix):
+def blur(sub_matrix,method='gaussian'):
+    """
+    This function receives the sub_array from find_face
+    and given a blur method, it applies that blur to that
+    sub_array, and returns the blurred sub_array.
+    """
     pass
--- src/recomp.py
+++ src/recomp.py
@@ -1,2 +1,7 @@
 def recomp(matrix,sub_matrix):
+    """
+    This function receives the original image in an array
+    form (matrix), and the sub_array (sub_matrix), and replaces
+    the old array by adding the new sub_array on top of the old array.
+    """
     pass
